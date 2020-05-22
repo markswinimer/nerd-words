bcb4be12004053fc1c549c0fb72750ac6d6bb566
markswinimer <markswinimer@gmail.com>
2020-05-22 11:59:43 -0400

    adds user model and create feature

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = mongoose.model('User', {
-    userName: {
+    name: {
         type: String,
         required: true,
         trim: true,
-        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid.')
+            }
+        }
     },
     password: {
         type: String,
--- routers/user.js
+++ routers/user.js
@@ -3,6 +3,20 @@ const User = require('../models/user');
 
 const router = new express.Router()
 
+router.post('/users', async (req, res) => {
+    console.log("---Attempting Create User---")
+    console.log(req.body)
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        console.log(e)
+        res.status(400).send()
+    }
+})
+
 router.post('/login', async (req, res) => {
     console.log("---Attempting Login---")
     console.log(req.body)
